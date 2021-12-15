b076dee97def80038a547df1ea82651385389199
Andrey <andreyyy.korsakov@mail.ru>
2021-12-15 18:13:22 +0300

    function)

--- javScript.js
+++ javScript.js
@@ -1,27 +1,59 @@
 
-var appData={
-    budjet: 0,
-    timeData: "not",
-    Savings:false
+let money,time;
+function start(){
+   money = prompt("Ваш бюджет на месяц?","денег нет" )
+   time = prompt("Введите дату в формате YYYY-MM-DD?","2000-00-00" )
+   while(isNaN(money) || money=="" || money == null){
+    money = prompt("Ваш бюджет на месяц?","денег нет" );
+   }
 }
-var expenses ={}
-var optionalExpenses ={}
-let arr=["1","2"]
-var money = prompt("Ваш бюджет на месяц?","денег нет" )
-var time = prompt("Введите дату в формате YYYY-MM-DD?","2000-00-00" )
+
+start();
+
+var appData={
+    budjet: money,
+    timeData: time,
+    Savings:true,
+    income: [],
+    expenses :{},
+    optionalExpenses :{}
+}
+/*
+function choseExpenses(){
+ for(let i=0;i<2;i++){
+     var firstcveshen = prompt("Введите обязательную статью расходов","_" ),secondcveshen=prompt("Введите стоимость","$" );
+     if((typeof(firstcveshen)==="string")  && typeof(firstcveshen)!= null && typeof(secondcveshen)!=null && firstcveshen!=" " && secondcveshen!=" " && firstcveshen.length<30){
+        console.log("[+]");
+        appData.expenses[firstcveshen]=secondcveshen;
+     }else{
+         alert("[-]");
+        i--;
+}
+ }
+}
+choseExpenses();
+
+appData.moneyPerDay=((appData.budjet/30).toFixed(1));
+console.log(appData.moneyPerDay);
+if(parseInt(appData.moneyPerDay)<100){
+    console.log("No Money No Funny")
+}else if(parseInt(appData.moneyPerDay)>100 && parseInt(appData.moneyPerDay)<2000){
+    console.log("No Money , But Funny")
+}else if(parseInt(appData.moneyPerDay)>2000){
+    console.log("Have Money , And Funny")
+}else{
+    console.log("[-]")
+}
+
+*/
+function CheckSavings(){
+    if(appData.Savings==true){
+        var save = +prompt("Ваши накопления?","денег нет" ) ; 
+        var persent = +prompt("Какой процент?");
+    }
+    appData.monthIncome=save/100/12*persent;
+    alert("Доход в месяц с депозита="+appData.monthIncome);
+}
+CheckSavings()
 
 
-appData.budjet=money
-appData.timeData=time
-
-var firstcveshen = prompt("Введите обязательную статью расходов","_" )
-expenses[firstcveshen] = prompt("Введите стоимость","$" )
-
-var Secondcveshen = prompt("Введите обязательную статью расходов","_" )
-expenses[Secondcveshen] = prompt("Введите стоимость","$" )
-
-
-console.log(expenses);
-console.log(appData);
-//console.log(parseInt(appData.timeData[0]));
-alert(money/30);
